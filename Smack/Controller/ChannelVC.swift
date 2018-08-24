bbde8a0ed1f41b03e46fd8de8f3501b17363c54b
Eli Armstrong MACBOOKPRO <ELIJEFFREYARMSTRONG@GMAIL.COM>
2018-08-24 13:21:01 -0700

    added reveal gestures to reveal channels

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Eli Armstrong on 8/24/18.
+//  Copyright © 2018 Eli Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width  - 60;
+        //print(self.view.frame.size.width  - 60)
+    }
+
+
+}
