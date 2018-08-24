081371b366e5f249f77819ec6984642757c9acdf
Eli Armstrong MACBOOKPRO <ELIJEFFREYARMSTRONG@GMAIL.COM>
2018-08-24 16:13:16 -0700

    Added LoginVC and UI elements

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Eli Armstrong on 8/24/18.
+//  Copyright © 2018 Eli Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utillities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Eli Armstrong on 8/24/18.
+//  Copyright © 2018 Eli Armstrong. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
+
