46661afae296f2e9b31d4827509152c15fb629e5
Eli Armstrong MACBOOKPRO <ELIJEFFREYARMSTRONG@GMAIL.COM>
2018-08-28 10:37:17 -0700

    Created UI function to sent post to API

---
+++ Smack/View/roundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  roundedButton.swift
+//  Smack
+//
+//  Created by Eli Armstrong on 8/28/18.
+//  Copyright © 2018 Eli Armstrong. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class roundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0{
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView(){
+        self.layer.cornerRadius = cornerRadius
+    }
+}
