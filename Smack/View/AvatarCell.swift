ea05a0e6f71f5bcd20d3310a2114d2d4a67992cf
Eli Armstrong MACBOOKPRO <ELIJEFFREYARMSTRONG@GMAIL.COM>
2018-09-14 14:34:43 -0700

    added avatar cells

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,25 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Eli Armstrong on 9/14/18.
+//  Copyright © 2018 Eli Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView(){
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
