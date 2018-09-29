055dfacfe27f2b0c1d66a307696a0b15cb0be122
Eli Armstrong MACBOOKPRO <ELIJEFFREYARMSTRONG@GMAIL.COM>
2018-09-28 20:17:55 -0700

    channels add to table view

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
-
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
-    
     @IBOutlet weak var userImg: Circleimage!
+    @IBOutlet weak var tableView: UITableView!
     
     // This make it so that an exit segue (the exit button at the top of view controllers) will be able to use this func as a destination.
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
@@ -21,6 +21,9 @@ class ChannelVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // This will cause all but 60 pixels to reveal of this view controller
         //print(self.view.frame.size.width  - 60)
@@ -58,6 +61,24 @@ class ChannelVC: UIViewController {
             loginBtn.setTitle("Login", for: .normal)
             userImg.image = UIImage(named: "menuProfileIcon")
             userImg.backgroundColor = UIColor.clear
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return UITableViewCell()
         }
     }
     
